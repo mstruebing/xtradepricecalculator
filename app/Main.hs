840b6169f5c6a659d7090411f50bc899e3a1d303
Max Strübing <mxstrbng@gmail.com>
2017-09-21 11:22:45 +0200

    calculate average price

--- app/Main.hs
+++ app/Main.hs
@@ -22,17 +22,27 @@ main = do
     let food = dropWhile (/= "--food") args
     let steel= dropWhile (/= "--steel") args
     let electronics = dropWhile untilNextArgument $ args
-    let ironAmount = takeWhile untilNextArgument $ tail iron
-    let waterAmount = takeWhile untilNextArgument $ tail water
-    let foodAmount = takeWhile untilNextArgument $ tail food
-    let steelAmount = takeWhile untilNextArgument $ tail steel
-    let electronicsAmount = takeWhile untilNextArgument $ tail electronics
+    let ironList = takeWhile untilNextArgument $ tail iron
+    let ironAmount = sum $ map (\x -> read x :: Double) ironList
+    let waterList = takeWhile untilNextArgument $ tail water
+    let waterAmount = sum $ map (\x -> read x :: Double) waterList
+    let foodList = takeWhile untilNextArgument $ tail food
+    let foodAmount = sum $ map (\x -> read x :: Int) foodList
+    let steelList = takeWhile untilNextArgument $ tail steel
+    let steelAmount = sum $ map (\x -> read x :: Int) steelList
+    let electronicsList = takeWhile untilNextArgument $ tail electronics
+    let electronicsAmount = sum $ map (\x -> read x :: Int) electronicsList
 
-    mapM putStrLn args
-    putStrLn "EHLO"
-    mapM putStrLn iron
-    putStrLn "EHLO"
-    mapM putStrLn ironAmount
+    let ironPrice = ironAmount / (fromIntegral $ length ironList)
+    let waterPrice = waterAmount / (fromIntegral $ length waterList)
+    let foodPrice = ironAmount / (fromIntegral $ length ironList)
+    let steelPrice = waterAmount / (fromIntegral $ length waterList)
+
+    putStrLn $ show ironPrice 
+    putStrLn $ show waterPrice
+    putStrLn $ show foodAmount
+    putStrLn $ show steelAmount
+    putStrLn $ show electronicsAmount
 
     where
         untilNextArgument x = head x /= '-'
