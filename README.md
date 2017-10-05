29f3a36bb3ea82f511c16d2ca06adc8ad19a8b09
Max Strübing <mxstrbng@gmail.com>
2017-10-05 16:24:53 +0200

    current implementation

---
+++ README.md
@@ -0,0 +1,49 @@
+# XTradePriceCalculator
+
+This is a small HTTP-service to calculate the prices of the goods for the XTrade game 
+in dependence of the current available amounts of goods
+
+## Requirements
+
+[PHP 7.1 is required as type hints inclusive void is used.](https://wiki.php.net/rfc/void_return_type)
+
+## Usage 
+
+To use this service you have to make an HTTP-request with five different GET-parameters
+where everyone has to be set.
+The five parameters are: `iron`, `water`, `food`, `steel` and `electronics` and 
+every single one have to be an integer value.
+
+The correct request address and port is dependent on where this service is running.
+
+In case it is running on localhost on port 8000 it could be called like this:
+
+`curl '127.0.0.1:8000?iron=30&water=5&food=2&steel=50000&electronics=100'`
+
+## Development
+
+For Development please make sure that your editor reads the `.editorconfig`.
+Also `composer` should be used to comfortable install and use some code quality tools.
+Currently two different code quality tools are used `editorconfig-checker` to make sure 
+all files are respecting the `.editorconfig` and `PHP_CodeSniffer` to make sure 
+the code implements the PSR2 standard. 
+Because of the small size of this service we currently violate one rule 
+but this should stay exactly at this number.
+To install these dependencies it is enough to do a simple `composer install`.
+To invoke the both tools you could just type `composer lint` int your terminal.
+To invoke just one you could either type `composer lint:editorconfig` or `composer lint:psr2`.
+
+The mentioned error which is expected is this:
+
+```
+A file should declare new symbols (classes, functions,
+constants, etc.) and cause no other side effects, or
+it should execute logic with side effects, but should
+not do both. The first symbol is defined on line 19
+and the first side effect is on line 106.
+```
+
+Further information to the mentioned tools:
+[Editorconfig](http://editorconfig.org/)
+[editorconfig-checker](https://github.com/editorconfig-checker/editorconfig-checker.php)
+[PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer)
--- index.php
+++ index.php
@@ -1,26 +1,105 @@
 <?php
 
-const EULER = 2.7182818284590452354;
-
+/* Default prices of different goods */
 const IRON_DEFAULT_PRICE = 15;
 const WATER_DEFAULT_PRICE = 20;
 const FOOD_DEFAULT_PRICE = 20;
 const STEEL_DEFAULT_PRICE = 70;
 const ELECTRONICS_DEFAULT_PRICE = 180;
 
-function calculate(int $amount, int $defaultPrice) : int {
-    return 1 + (($defaultPrice - 1) * pow(EULER, (-0.01 * $amount)));
+/* Threshold in percent when a good is */
+const THRESHOLD = 20;
+
+/*
+ * Checks if a request contains all needed information for this service
+ *
+ * @param array $args - the $_GET variables
+ * @return bool - whether the request contains all valid information or not
+ */
+function isValidRequest(array $args) : bool
+{
+    return count($args) === 5 &&
+        isset($args['iron']) && is_numeric($args['iron']) &&
+        isset($args['water']) && is_numeric($args['water']) &&
+        isset($args['food']) && is_numeric($args['food']) &&
+        isset($args['steel']) && is_numeric($args['steel']) &&
+        isset($args['electronics']) && is_numeric($args['electronics']);
 }
 
-function main() {
-    if (isset($_GET['iron']) && isset($_GET['water']) && isset($_GET['food']) && isset($_GET['steel']) && isset($_GET['electronics'])) {
-        $ironPrice = calculate($_GET['iron'], IRON_DEFAULT_PRICE);
-        $waterPrice = calculate($_GET['water'], WATER_DEFAULT_PRICE);
-        $foodPrice = calculate($_GET['food'], FOOD_DEFAULT_PRICE);
-        $steelPrice = calculate($_GET['steel'], STEEL_DEFAULT_PRICE);
-        $electronicsPrice = calculate($_GET['electronics'], ELECTRONICS_DEFAULT_PRICE);
+/*
+ * Calculates the price of a good
+ *
+ * @param int $amount - the amount of the good
+ * @param int $defaultPrice - the default price for the specific good
+ * @return int - the price
+ */
+function calculatePrice(int $amount, int $defaultPrice) : int
+{
+    return 1 + (($defaultPrice - 1) * pow(M_E, (-0.01 * $amount)));
+}
 
-        printf ("iron: %s, water; %s, food: %s, steel: %s, electronics: %s\n", $ironPrice, $waterPrice, $foodPrice, $steelPrice, $electronicsPrice);
+/**
+ * Calculates adjusted default prices out of different amounts of goods
+ *
+ * @param int $ironAmount
+ * @param int $waterAmount
+ * @param int $foodAmount
+ * @param int $steelAmount
+ * @param int $electronicsAmount
+ * @return array - an array containing the new calculated prices
+ */
+function adjustDefaultPrices(
+    int $ironAmount,
+    int $waterAmount,
+    int $foodAmount,
+    int $steelAmount,
+    int $electronicsAmount
+) : array {
+    return [];
+}
+
+/**
+ * Main function
+ */
+function main() : void
+{
+    $args = $_GET;
+
+    if (isValidRequest($args)) {
+        $ironAmount = $args['iron'];
+        $waterAmount = $args['water'];
+        $foodAmount = $args['food'];
+        $steelAmount = $args['steel'];
+        $electronicsAmount = $args['electronics'];
+
+        $defaultPrices = adjustDefaultPrices($ironAmount, $waterAmount, $foodAmount, $steelAmount, $electronicsAmount);
+
+        if (!isset($defaultPrices['iron']) &&
+            !isset($defaultPrices['water']) &&
+            !isset($defaultPrices['food']) &&
+            !isset($defaultPrices['steel']) &&
+            !isset($defaultPrices['electronics'])) {
+            printf("ERROR: Something went wrong code wise\n");
+        }
+
+        $ironPrice = calculatePrice($ironAmount, $defaultPrices['iron']);
+        $waterPrice = calculatePrice($waterAmount, $defaultPrices['water']);
+        $foodPrice = calculatePrice($foodAmount, $defaultPrices['food']);
+        $steelPrice = calculatePrice($steelAmount, $defaultPrices['steel']);
+        $electronicsPrice = calculatePrice($electronicsAmount, $defaultPrices['electronics']);
+
+        printf(
+            "iron: %s, water; %s, food: %s, steel: %s, electronics: %s\n",
+            $ironPrice,
+            $waterPrice,
+            $foodPrice,
+            $steelPrice,
+            $electronicsPrice
+        );
+    } else {
+        printf(
+            "ERROR: You have to call this service with iron, water, food, steel and electronics as numeric values\n"
+        );
     }
 }
 
